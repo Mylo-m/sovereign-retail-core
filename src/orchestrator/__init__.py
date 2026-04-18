92e0b5063abc186c17a795813fcda4046d50b7fb
Kamil Meer-Motala <92265417+Mylo-m@users.noreply.github.com>
2026-04-18 22:47:09 +0200

    Update __init__.py

--- src/orchestrator/__init__.py
+++ src/orchestrator/__init__.py
@@ -1,1 +1,11 @@
 # Initialize the orchestrator module
+
+
+ __  __ _   _ _     ___  
+|  \/  | | | | |   / _ \ 
+| |\/| | |_| | |__| (_) |
+|_|  |_|\__, |_____\___/ 
+        |___/            
+BUIDING SOVEREIGN SOLUTIONS
+_    ___ __ ___ _____ _   _ 
+W W W . M Y L O . C O . Z A
